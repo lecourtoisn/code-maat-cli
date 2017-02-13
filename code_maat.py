21888f7e62c7a433513e0c30604a5558497096b7
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-02-13 19:31:21 +0100

    Add methods to make stats out of code-maat raw data

--- code_maat.py
+++ code_maat.py
@@ -1,5 +1,9 @@
 import os
 from enum import Enum
+import csv
+
+import math
+from pprint import pprint
 
 from git import Repo
 
@@ -7,35 +11,37 @@ PROJECT_DIR = "projects"
 
 
 class ANALYSIS(Enum):
-    abs_churn = "abs-churn"
     age = "age"
-    author_churn = "author-churn"
-    authors = "authors"
-    communication = "communication"
     coupling = "coupling"
-    entity_churn = "entity-churn"
-    entity_effort = "entity-effort"
-    entity_ownership = "entity-ownership"
     fragmentation = "fragmentation"
     identity = "identity"
-    main_dev = "main-dev"
-    main_dev_by_revs = "main-dev-by-revs"
-    messages = "messages"
-    refactoring_main_dev = "refactoring-main-dev"
     revisions = "revisions"
     soc = "soc"
-    summary = "summary"
+    # abs_churn = "abs-churn"
+    # author_churn = "author-churn"
+    # authors = "authors"
+    # communication = "communication"
+    # entity_churn = "entity-churn"
+    # entity_effort = "entity-effort"
+    # entity_ownership = "entity-ownership"
+    # main_dev = "main-dev"
+    # main_dev_by_revs = "main-dev-by-revs"
+    # messages = "messages"
+    # refactoring_main_dev = "refactoring-main-dev"
+    # summary = "summary"
 
 
 class Project:
-    def __init__(self, git_url):
+    def __init__(self, git_url=None, folder_name=None):
         self.git_url = git_url
-        self.name = git_url.split("/")[-1].split(".")[0]
+        self.name = git_url.split("/")[-1].split(".")[0] if git_url is not None else folder_name
         self.path = os.path.join(PROJECT_DIR, self.name)
         self.log_path = os.path.join(self.path, 'data.log')
         self.results = os.path.join(self.path, 'results')
 
     def clone(self, force=False):
+        if not self.git_url:
+            raise Exception("Url unknown")
         if force or not os.path.exists(self.path):
             os.system("git clone {} {}".format(self.git_url, self.path))
         if not os.path.exists(self.results):
@@ -51,16 +57,17 @@ class Project:
         output = os.path.join(self.results, output)
         self._run_code_maat(output, a=analysis.value)
 
-    def count_keyword(self, keywords: list):
+    def count_keyword(self, keywords: list = None):
+        keywords = keywords or ["add", "fix", "bug",
+                                "test", "refactor", "issue", "remove", "rm",
+                                ]
         results = {}
         for keyword in keywords:
             repo = Repo(self.path)
             commits = list(repo.iter_commits('master'))
             matching = [c.message for c in commits if keyword in c.message]
             results[keyword] = len(matching)
-
-        with open(os.path.join(self.results, 'keywords.csv'), 'w+') as result_file:
-            result_file.writelines(["{};{}\n".format(key, value) for key, value in results.items()])
+        print("{:10} {}".format(self.name, ', '.join(["{} {:10}".format(key, "{}%({})".format(int(value*100/len(commits)), value)) for key, value in results.items()])))
 
     def _run_code_maat(self, output, **arguments):
         args = ' '.join(["-{} {}".format(arg, value) for arg, value in arguments.items()])
@@ -68,3 +75,42 @@ class Project:
                "> {}".format(self.log_path, args, output))
         print(cmd)
         os.system(cmd)
+
+    def get_result(self, analysis):
+        return os.path.join(self.results, "{}.csv".format(analysis))
+
+    def result_iterator(self, analysis):
+        with open(self.get_result(analysis)) as file:
+            file = csv.DictReader(file)
+            for row in file:
+                yield row
+
+    def age_routine(self):
+        nb_files = 0
+        recent_files = 0
+        for row in self.result_iterator("age"):
+            path, age = os.path.join(self.path, row['entity']), int(row['age-months'])
+            if not os.path.exists(path):
+                continue
+            nb_files += 1
+            if age <= 2:
+                recent_files += 1
+        print("{:10}{:6}{:6}{:6}%".format(self.name, recent_files, nb_files, math.floor(recent_files * 100 / nb_files)))
+
+    def age_routine_avg(self):
+        max_age = max(int(x['age-months']) for x in self.result_iterator("age"))
+        percents = [int(x['age-months'])/max_age for x in self.result_iterator("age")]
+        average = sum(percents) * 100 / len(percents)
+        print("{:10} {:3.2f}%".format(self.name, average))
+
+    def revisions_routine(self):
+        # revisions = [row['n-revs'] for row in self.result_iterator("revisions")]
+        revisions = list(self.result_iterator("revisions"))
+        get_revisions = lambda x: int(x['n-revs'])
+        max_rev = max(revisions, key=get_revisions)
+        avg_rev = sum([get_revisions(a) for a in revisions]) / len(revisions)
+        print("{:10}{:6}{:6.2f}".format(self.name, get_revisions(max_rev), avg_rev))
+
+    def get_nb_commits(self):
+        repo = Repo(self.path)
+        print(self.name, len(list(repo.iter_commits('master'))))
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from cmd import Cmd
 
 from code_maat import Project, ANALYSIS
@@ -7,8 +8,8 @@ class Shell(Cmd):
     prompt = ':'
 
     @staticmethod
-    def do_inspect(git):
-        """Generates stats for git repository given in parameters"""
+    def do_retrieve(git):
+        """Retrieve projects and perform code-maat analyses for git repository given in parameters"""
         project = Project(git)
 
         print("Cloning project if it doesn't exist")
@@ -21,11 +22,32 @@ class Shell(Cmd):
 
         for analysis in ANALYSIS:
             project.run_analysis(analysis=analysis)
-        project.count_keyword(["add", "fix", "bug", "test", "refactor"])
         print("Analysis done")
+
+    @staticmethod
+    def do_analyse(projects):
+        """Aggregates code-mat raw data of specified projects to print statistics
+        ex: analyse fitnesse gson"""
+        if not projects:
+            projects = os.listdir('projects')
+        else:
+            projects = projects.split(' ')
+
+        print(projects)
+        stats = [Project.age_routine, Project.age_routine_avg, Project.revisions_routine, Project.get_nb_commits,
+                 Project.count_keyword]
+        for stat in stats:
+            print(stat.__name__)
+            for project in projects:
+                p = Project(folder_name=project)
+                stat(p)
+            print("_______________")
 
 
 if __name__ == '__main__':
     shell = Shell()
-    # shell.onecmd("inspect https://github.com/google/gson.git")
+    # shell.onecmd("retrieve https://github.com/google/gson.git")
+    # shell.onecmd("analyse")
+    # shell.onecmd("analyse fitnesse")
+    # shell.onecmd("analyse fitnesse gson")
     shell.cmdloop()
